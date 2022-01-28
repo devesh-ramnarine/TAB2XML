c970a7887e17b29767be08366b24a83bfe8add28
Ion Dobridniuc <iond98@my.yorku.ca>
2022-01-28 18:05:23 -0500

    develop

--- src/main/java/GUI/MainViewController.java
+++ src/main/java/GUI/MainViewController.java
@@ -42,7 +42,7 @@ import javafx.stage.Stage;
 import javafx.stage.Window;
 import utility.Range;
 import utility.Settings;
-
+// Test for develop branch
 public class MainViewController extends Application {
 	
 	private Preferences prefs;
