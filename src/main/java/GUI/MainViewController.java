ec2b642891b7cb2879639e0bcfe19329897354a8
Safiyyah Patel <safi18@my.yorku.ca>
2022-01-28 19:06:51 -0500

    testing conflict

--- src/main/java/GUI/MainViewController.java
+++ src/main/java/GUI/MainViewController.java
@@ -44,7 +44,7 @@ import utility.Range;
 import utility.Settings;
 // Test for develop branch
 // Ion change
-//Ahmed test for develop branch
+//Ahmed is testing develop branch
 
 // safiyyah - test for develop branch
 public class MainViewController extends Application {
